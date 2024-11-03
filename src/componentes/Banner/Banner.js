bf1928b7aebf0004b73162544745cfe02446354a
PedroPauloBorgesMizael <pedropaulo.bm2006@gmail.com>
2024-11-03 18:09:20 -0300

    primeiro commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,26 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <div className="fotos__banner">
+        <img src="/imagens/foto1.png" alt="Foto pessoal" />
+        <img src="/imagens/foto2.png" alt="Foto pessoal" />
+      </div>
+      <div className="texto__banner">
+        <h1>
+          MEU
+          <br />
+          PORTIFÓLIO
+        </h1>
+        <p>
+          Pedro Paulo Borges Mizael
+          <br />
+          Iniciando em Front-end
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Banner;
---
+++ src/componentes/Card/Card.js
@@ -0,0 +1,12 @@
+import './Card.css'
+
+export default function Card(area, id, descricao, img) {
+    return (
+        <section className='container__card'>
+            <h2>{area}</h2>
+            <div>
+                
+            </div>
+        </section>
+    );
+}
